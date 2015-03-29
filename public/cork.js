50fca5aee56206d4b8c61013083963b31ab8d3e2
A. Dorsk <adorsk@gmail.com>
2015-03-28 20:09:52 -0400

    start of image iterator

--- public/cork.js
+++ public/cork.js
@@ -1,28 +1,56 @@
-var canvas = document.getElementById('myCanvas');
-var context = canvas.getContext('2d');
+var outputCanvas = document.getElementById('myCanvas');
+var outputCtx = canvas.getContext('2d');
 
-function drawImage(imageObj) {
-    var imageWidth = imageObj.width;
-    var imageHeight = imageObj.height;
-    canvas.width = imageWidth;
-    canvas.height = imageHeight;
+/**
+ * ImageIterator: takes an imageObject.
+ * next() function returns rectangles of pixel
+ * data, starting from the top-left, and going
+ * left-to-right, top-to-bottom.
+ * When image has been iterated through, next returns
+ * null.
+ */
+function ImageIterator(imageObj) {
+    this.imageObj = imageObj;
 
-    context.drawImage(imageObj, 0, 0);
+    // @TODO: hardcoded opts for now, 
+    // take as options later.
+    this.opts = {
+        rectWidth: 20,
+        rectHeight: 20
+    };
 
-    var imageData = context.getImageData(0, 0, imageWidth, imageHeight);
-    var data = imageData.data;
+    this._x = 0;
+    this._y = 0;
+};
+ImageIterator.prototype.next = function() {
+    // @TODO: check if past bounds.
 
-    // iterate over all pixels
-    for(var i = 0, n = data.length; i < n; i += 4) {
-        var red = data[i];
-        var green = data[i + 1];
-        var blue = data[i + 2];
-        var alpha = data[i + 3];
-    }
+    // Get rectangle bounds.
+    var x0 = this._x;
+    var x1 = Math.max(x0 + this.opts.rectWidth, this.imageObj.width);
+    var y0 = this._y;
+    var y1 = y0 + Math.max(this.opts.rectHeight, this.imageObj.height);
+
+    // Get data for rectangle.
+    var rectData = [];
+    for (var x=x0; x <
+
+    // Update current coords.
+    this._x = x1 + 1;
+    this._y = y1 + 1;
+};
+
+function getImageCanvas(imageObj) {
+    var canvas = document.createElement('canvas');
+    canvas.width = imageObj.width;
+    canvas.height = imageObj.height;
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(imageObj, 0, 0);
+    return imgCanvas;
 }
 
 var imageObj = new Image();
 imageObj.onload = function() {
-    drawImage(this);
+    var canvas = getImageCanvas(this);
 };
 imageObj.src = '/testImages/testImage.jpg';
