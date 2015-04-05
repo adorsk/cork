a56f3903b1434232d25fa14bac6267e807d964af
A. Dorsk <adorsk@gmail.com>
2015-04-05 18:23:13 -0400

    get rgba data for tiles

--- public/cork.js
+++ public/cork.js
@@ -11,6 +11,8 @@
 class CanvasIterator {
     constructor(canvas, opts) {
         this.canvas = canvas;
+        this.ctx = this.canvas.getContext('2d');
+
         // @TODO: hardcoded opts for now, 
         // take as options later.
         this.opts = {
@@ -29,30 +31,50 @@ class CanvasIterator {
         }
 
         // Move to next row if past the edge.
-        if (this._x > this.canvas.width) {
+        if (this._x >= this.canvas.width) {
             this._x = 0;
             this._y += this.opts.tileHeight;
         }
 
         // If past the bottom edge, mark as done.
-        if (this._y > this.canvas.height) {
+        if (this._y >= this.canvas.height) {
             this._done = true;
             return [this._done, null];
         }
 
         // Get tile bounds.
         let x0 = this._x;
-        let x1 = Math.min(x0 + this.opts.tileWidth -1, this.canvas.width);
+        let x1 = Math.min(x0 + this.opts.tileWidth - 1, this.canvas.width);
         let y0 = this._y;
-        let y1 = y0 + Math.min(this.opts.tileHeight, this.canvas.height);
+        let y1 = Math.min(y0 + this.opts.tileHeight - 1, this.canvas.height);
+
+        var curTileHeight = x1 - x0;
+        var curTileWidth = y1 - y0;
 
         // Get data for tile.
+        let pixelData = this.ctx.getImageData(x0, y0, curTileWidth, curTileHeight).data;
+        let pixelIdx = 0;
+        let rows = [];
+        for (let y=y0; y < y1; y++ ){
+            let row = [];
+            for (let x=x0; x < x1; x++) {
+                var rgba = [];
+                for (var i=0; i<4; i++) {
+                    rgba.push(pixelData[pixelIdx+i]);
+                }
+                row.push(rgba);
+                pixelIdx += 4;
+            }
+            rows.push(row);
+        }
+
+        // Package tile data.
         let tileData = {
             x0: x0,
             x1: x1,
             y0: y0,
             y1: y1,
-            data: []
+            data: rows
         };
 
         // Update current coords.
