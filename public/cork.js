69d77727b6ecf3ab5d7f22f9fff51aac2ce6683c
A. Dorsk <adorsk@gmail.com>
2015-04-05 19:36:18 -0400

    start of cork filter

--- public/cork.js
+++ public/cork.js
@@ -99,6 +99,62 @@ function copyTile(ctx, tileData) {
     }
 }
 
+/**
+ * Converts an RGB color value to HSL. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes r, g, and b are contained in the set [0, 255] and
+ * returns h, s, and l in the set [0, 1].
+ *
+ * @param   Number  r       The red color value
+ * @param   Number  g       The green color value
+ * @param   Number  b       The blue color value
+ * @return  Array           The HSL representation
+ */
+function rgbToHsl(r, g, b){
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if(max == min){
+        h = s = 0; // achromatic
+    }else{
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max){
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    return [h, s, l];
+}
+
+/**
+ * Copy tile as if it was stamped on via cork stamp.
+**/
+function corkifyTile(ctx, tileData) {
+    // Get average luminosity of tile.
+    var totalLuminosity = 0;
+    for (var i=0; i<tileData.pixels.length; i++) {
+        // Get pixel data.
+        var pixel = tileData.pixels[i];
+        var [h,s,l] = rgbToHsl(pixel[1]);
+        totalLuminosity += l;
+    }
+    var avgLuminosity = totalLuminosity/tileData.pixels.length;
+
+    // Generate noise with that luminosity.
+    /*
+    var imgData = ctx.createImageData(1,1);
+    for (var j=0; j < 4; j++) {
+        imgData.data[j] = pixel[1][j];
+    }
+    ctx.putImageData(imgData,x,y);
+    */
+}
+
 function getImageCanvas(imageObj) {
     var canvas = document.createElement('canvas');
     canvas.width = imageObj.width;
