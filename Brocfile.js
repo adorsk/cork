6afa6fd7853b2494c76b3ed537708e7ab24b9694
A. Dorsk <adorsk@gmail.com>
2015-04-05 19:47:45 -0400

    add browserify to broccoli

--- Brocfile.js
+++ Brocfile.js
@@ -1,14 +1,22 @@
-var esTranspiler = require('broccoli-babel-transpiler');
 var pickFiles = require('broccoli-static-compiler');
 var mergeTrees = require('broccoli-merge-trees');
+var browserify = require('broccoli-fast-browserify');
+var babelify = require('babelify');
 
 
 var jsTree = pickFiles('public/', {
-  srcDir: '/',
-  files: ['**/*.js',],
-  destDir: '/'
+    srcDir: '/',
+    files: ['**/*.js',],
+    destDir: '/'
 });
 
-jsTree = esTranspiler(jsTree);
+jsTree = browserify(jsTree, {
+    bundles: {
+        'cork.js': {
+            entryPoints: ['**/*.browserify.js'],
+            transform: [babelify]
+        }
+    }
+});
 
 module.exports = mergeTrees(['public/', jsTree], {overwrite: true});
